25f2aff28980d926bf5002fcea296fb805ab1754
DevLama <hlama@live.fr>
2018-11-05 10:17:23 +0100

    Add trash data for cancel Orders or Billing or Booking foods

---
+++ src/com/openfoods/controllers/trashController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openfoods.controllers;
+
+import com.openfoods.configs.ActionType;
+import com.openfoods.configs.SQLDB;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author hornellama
+ */
+public class trashController {
+    private SQLDB sqldb=null;
+    public static List<Map<String,Object>> arrayData;
+    public boolean setDataInTrash(ActionType actionType) throws ClassNotFoundException, SQLException{
+        LocalDate date=LocalDate.now();
+        sqldb=new SQLDB();
+        String query="";
+        switch(actionType){
+            case BOOKING:
+                for (Map<String, Object> map : arrayData) {
+                    query="INSERT INTO (invoiceid,datecmd,qte,idfood,foodname,price,datetrash,type) VALUES ('"+map.get("invoiceid")+"','"+map.get("datecmd")+"','"+map.get("qte")+"','"+map.get("idprod")+"',,'"+map.get("nameprod")+"','"+date.toString()+"','BOOKING')";
+                    sqldb.setQueryUpdate(query);
+                }
+                
+                
+                break;
+                
+            case ORDER:
+                for (Map<String, Object> map : arrayData) {
+                    query="INSERT INTO (invoiceid,datecmd,qte,idfood,foodname,price,datetrash,type) VALUES ('"+map.get("invoiceid")+"','"+map.get("datecmd")+"','"+map.get("qte")+"','"+map.get("idprod")+"',,'"+map.get("nameprod")+"','"+date.toString()+"','ORDER')";
+                    sqldb.setQueryUpdate(query);
+                }
+                break;
+        }
+        return true;
+    }
+    
+}
